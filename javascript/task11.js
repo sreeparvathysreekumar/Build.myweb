487ffcff6af6943ab1b9f6db3aeec454708b9ed7
sreeparvathysreekumar <sreeparvathysreekumar103@gmail.com>
2025-06-04 19:30:57 +0530

    Add files via upload

---
+++ javascript/task11.js
@@ -0,0 +1,9 @@
+function printPersonDetails(person) {
+    console.log(`Name: ${person.name}, Age: ${person.age}`);
+  }
+  
+  printPersonDetails({ name: "Alice", age: 25 }); 
+  
+  printPersonDetails({ name: "Bob", age: 30 }); 
+
+  
