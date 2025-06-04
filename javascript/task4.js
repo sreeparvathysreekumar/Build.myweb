5c788147bdeb41f85dae77ab579d300a7e77e32b
sreeparvathysreekumar <sreeparvathysreekumar103@gmail.com>
2025-06-04 19:15:58 +0530

    Add files via upload

---
+++ javascript/task4.js
@@ -0,0 +1,10 @@
+function findLargestNumber(numbers) {
+    let max = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+      if (numbers[i] > max) {
+        max = numbers[i];
+      }
+    }
+    return max;
+  }
+   console.log(findLargestNumber([10, 20, 5, 8])); 
