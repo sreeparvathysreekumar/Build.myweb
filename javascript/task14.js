e873dffa8d6386b054a2387f3ab762134cb34b28
sreeparvathysreekumar <sreeparvathysreekumar103@gmail.com>
2025-06-04 19:38:01 +0530

    Add files via upload

---
+++ javascript/task14.js
@@ -0,0 +1,22 @@
+function fizzBuzz(n) {
+    const result = [];
+  
+    for (let i = 1; i <= n; i++) {
+      if (i % 15 === 0) {
+        result.push("FizzBuzz");
+      } else if (i % 3 === 0) {
+        result.push("Fizz");
+      } else if (i % 5 === 0) {
+        result.push("Buzz");
+      } else {
+        result.push(i.toString());
+      }
+    }
+  
+    return result;
+  }
+  
+  
+  let n = 20;
+  let res = fizzBuzz(n);
+  console.log(res.join(' '));
