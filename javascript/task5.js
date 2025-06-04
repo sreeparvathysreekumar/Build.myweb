a693fd1f8b9481625396e5748ae9d69ec9f42448
sreeparvathysreekumar <sreeparvathysreekumar103@gmail.com>
2025-06-04 19:17:09 +0530

    Add files via upload

---
+++ javascript/task5.js
@@ -0,0 +1,8 @@
+function calculateSum(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      sum += numbers[i];
+    }
+    return sum;
+  }
+   console.log(calculateSum([1, 2, 3, 4, 5]));  
